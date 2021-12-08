1e41568e7beab417dfad0a752ec7169b0f7e3057
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2021-12-07 23:41:29 -0800

    Second commit
    Changed routing and created validation to connect with the frontend of the application.

---
+++ src/observations/observations.controller.js
@@ -0,0 +1,75 @@
+const service = require("./observations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+const observations = []
+
+const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
+
+function hasData(req, res, next) {
+  if (req.body.data) {
+    return next()
+  }
+  next({status: 400, message: "body must have data property"})
+}
+
+function hasLatitude(req, res, next) {
+  const latitude = Number(req.body.data.latitude)
+  if (latitude >= -90 && latitude <= 90 ) {
+    return next()
+  }
+  next({status: 400, message: "latitude must be between -90 and 90"})
+}
+
+function hasLongitude(req, res, next) {
+  const longitude = Number(req.body.data.longitude)
+  if (longitude >= -180 && longitude <= 180 ) {
+    return next()
+  }
+  next({status: 400, message: "longitude must be between -180 and 180"})
+}
+
+function hasValidAirTemperature(req, res, next) {
+    const airTemperature = Number(req.body.data.airTemperature)
+    const airTemperatureUnit = req.body.data.airTemperatureUnit
+    
+    if (airTemperatureUnit === "C" && airTemperature >= -50 && airTemperature <= 107) {
+       return next()
+    }
+
+    if (airTemperatureUnit === "F" && airTemperature >= -60 && airTemperature <= 224) {
+        return next()
+    }
+
+    next({status: 400, message: `Please enter a valid temperature value`})
+}
+
+function hasSkyCondition(req, res, next) {
+  const skyCondition = Number(req.body.data.sky_condition)
+
+  if (validSkyConditions.includes(skyCondition)) {
+    return next()
+  }
+  next({status: 400, message: `sky_condition must be one of: ${validSkyConditions}`})
+}
+
+
+async function create(req, res) {
+  const newObservation = await service.create(req.body.data);
+
+
+  res.status(201).json({
+    data: newObservation,
+  });
+}
+
+async function list(req, res) {
+    const data = await service.list();
+      res.json({
+        data,
+      });
+    }
+
+module.exports = {
+  create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, asyncErrorBoundary(create)],
+  list: asyncErrorBoundary(list),
+};
